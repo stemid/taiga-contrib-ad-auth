e8fc387508a3e7d134fdf29df07f7952d2221610
Stefan Midjich <swehack@gmail.com>
2016-11-08 13:06:35 +0100

    simple testing.

---
+++ tests/test_login.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import pytest
+import sys
+from configparser import RawConfigParser
+
+# Locate taiga-back libs
+sys.path.append('../taiga-back/')
+
+from taiga_contrib_ad_auth import connector
+
+conf_defaults = {
+    'DEFAULT': {
+        'username': 'user@mydomain.local',
+        'password': 'secret.'
+    }
+}
+
+config = RawConfigParser(defaults=conf_defaults)
+config.readfp(open('tests_local.cfg'))
+
+def test_ad_login_success():
+    (email, username) = connector.login(
+        config.get('DEFAULT', 'username'),
+        config.get('DEFAULT', 'password')
+    )
